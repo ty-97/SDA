73611620947fbff03550f449ded7959916221b74
ty-97 <59327553+ty-97@users.noreply.github.com>
2022-02-17 10:48:23 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import os
 
 
 cmd = "/home/tanyi/miniconda3/envs/videonet/bin/python main.py somethingv2 RGB \
-     --model TSN --arch resnet50 --net SDA_full --num_segments 8 \
+     --model TSN --arch resnet50 --net SDA --num_segments 8 \
      --cdiv 4 \
      --gd 20 --lr 0.01 --lr_steps 20 40 --epochs 50 \
      --batch-size 36 -j 4 --dropout 0.5\
